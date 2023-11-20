c460213ce85c3386ea30ce5bfffbe089fb1f513d
Victor Wahlberg <wahlbergvictor01@gmail.com>
2023-11-20 10:24:07 +0100

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router";
+import { Navbar } from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import InvoicePage from "./pages/InvoicePage";
 import "./App.css";
 
-
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/invoice" element={<InvoicePage />} />
