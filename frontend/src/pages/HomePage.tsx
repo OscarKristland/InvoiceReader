854cd40cd33e280086357934eb8ac610c00abf64
Victor Wahlberg <wahlbergvictor01@gmail.com>
2023-11-20 11:51:35 +0100

    fixed my mistake

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,24 +1,18 @@
-
 import React, { useEffect, useState } from "react";
 
-
-
+interface Props {}
 
 const Home = (props: Props) => {
-
   const [invoiceList, setinvoiceList] = useState([]);
-    
-    useEffect(() => {
-        fetch('http://127.0.0.1:8000/get_invoice_list/')
-        .then(response => response.json())
-        .then(data => setinvoiceList(data))
-        .catch(error => console.error(error));
-        console.log(invoiceList)
-    }, []);
-  return (
-    <div className="bg-black">
-    </div>
-  );
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/get_invoice_list/")
+      .then((response) => response.json())
+      .then((data) => setinvoiceList(data))
+      .catch((error) => console.error(error));
+    console.log(invoiceList);
+  }, []);
+  return <div className="bg-black"></div>;
 };
 
 export default Home;
